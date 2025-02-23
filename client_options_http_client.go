f4984afc1d213f56a0f4d984d56ab6edbcb2738c
Flc <four_leaf_clover@foxmail.com>
2025-02-23 19:08:26 +0800

    Add initial implementation of Yuque API client and related structures

---
+++ client_options_http_client.go
@@ -0,0 +1,55 @@
+package yuque
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/hashicorp/go-retryablehttp"
+)
+
+type RetryableHTTPClientOption func(client *retryablehttp.Client)
+
+func WithRetryableHTTPClientLogger(logger retryablehttp.Logger) RetryableHTTPClientOption {
+	return func(client *retryablehttp.Client) {
+		client.Logger = logger
+	}
+}
+
+func WithRetryableHTTPClientRetryWaitMin(waitMin time.Duration) RetryableHTTPClientOption {
+	return func(client *retryablehttp.Client) {
+		client.RetryWaitMin = waitMin
+	}
+}
+
+func WithRetryableHTTPClientRetryWaitMax(waitMax time.Duration) RetryableHTTPClientOption {
+	return func(client *retryablehttp.Client) {
+		client.RetryWaitMax = waitMax
+	}
+}
+
+func WithRetryableHTTPClientRetryMax(retryMax int) RetryableHTTPClientOption {
+	return func(client *retryablehttp.Client) {
+		client.RetryMax = retryMax
+	}
+}
+
+func WithRetryableHTTPClientCheckRetry(checkRetry retryablehttp.CheckRetry) RetryableHTTPClientOption {
+	return func(client *retryablehttp.Client) {
+		client.CheckRetry = checkRetry
+	}
+}
+
+func WithRetryableHTTPClientBackoff(backoff retryablehttp.Backoff) RetryableHTTPClientOption {
+	return func(client *retryablehttp.Client) {
+		client.Backoff = backoff
+	}
+}
+
+func NewRetryableHTTPClient(opts ...RetryableHTTPClientOption) *http.Client {
+	retryClient := retryablehttp.NewClient()
+	retryClient.Logger = nil
+	for _, opt := range opts {
+		opt(retryClient)
+	}
+	return retryClient.StandardClient()
+}
